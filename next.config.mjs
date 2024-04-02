8bee1645acae2cd9d35145cdc36200bc14a53b8b
longpiseth2002 <stevkhat121212@gmail.com>
2024-04-02 19:07:28 +0700

    add more

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: [
-          '//store.istad.co/api/products/',
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: [
+                    'store.istad.co',
+                ]
+            },
         ]
-      },
-    ]
-  }
+    }
 }
